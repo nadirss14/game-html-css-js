f2c2e67401cb37308dcf035f68886ba3990fcdac
nadirss14 <nadirss14@gmail.com>
2019-12-27 17:43:38 -0600

    change path to public

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC23ZVNCFP05wafAhu346AzwerYuyw7YfY",
+  authDomain: "nadirss14-portfolio.firebaseapp.com",
+  databaseURL: "https://nadirss14-portfolio.firebaseio.com",
+  projectId: "nadirss14-portfolio",
+  storageBucket: "nadirss14-portfolio.appspot.com",
+  messagingSenderId: "683742480270",
+  appId: "1:683742480270:web:f3bc7042562cc6137e86df"
+};
+firebase.initializeApp(firebaseConfig);
